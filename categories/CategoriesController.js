d8922e1687efcefc5227c4f09b37bb077a3c30f6
Leonardo Albuquerque <leeodesign@hotmail.com>
2021-04-09 22:39:36 -0300

    Delete category

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -12,15 +12,31 @@ router.post("/admin/categories/save", (request, response) => {
   let title = request.body.title;
   let slug = slugify(title);
   if (title != undefined) {
-    Category.create({ title, slug }).then(() => { response.redirect("/") });
+    Category.create({ title, slug }).then(() => { response.redirect("/admin/categories/") });
   } else {
-    response.redirect("admin/categories/new");
+    response.redirect("/admin/categories/new");
 
   }
 });
 
 router.get("/admin/categories", (request, response) => {
-  response.render("");
+  Category.findAll().then((categories) => {
+    response.render("admin/categories/index", { categories });
+  });
+});
+
+router.post("/categories/delete", (request, response) => {
+  let id = request.body.id;
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Category.findOne({ where: { id } })
+        .then(() => {
+          Category.destroy({ where: { id } })
+        });
+    }
+  }
+  response.redirect("/admin/categories");
+
 });
 
 module.exports = router;
