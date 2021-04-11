36bfbe418e4adeadd3aa52a3ea86f3855a83fdff
Leonardo Albuquerque <leeodesign@hotmail.com>
2021-04-11 09:26:48 -0300

    Article Page

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/admin/article/new", (request, response) => {
-  response.send("Rota de Articles");
+  response.render("admin/articles/new");
 });
 
 module.exports = router;
